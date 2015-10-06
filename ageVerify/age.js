01277d54262530f5ec2a55f19ef747c2793f4f46
Travis Montgomery <travisbm@gmail.com>
2015-10-06 08:58:52 -0500

    wrote age verify function in ruby and javascript

---
+++ ageVerify/age.js
@@ -0,0 +1,8 @@
+var verify = function(age) {
+  if (age < 21)
+    console.log('Too Young sucka!');
+  else
+    console.log('Proceed.');
+};
+
+verify(22)
---
+++ ageVerify/age.rb
@@ -0,0 +1,9 @@
+class Verify
+  def verify(age)
+    if (age < 21)
+      puts 'Too Young Sucka!'
+    else
+      puts 'Proceed.'
+    end
+  end
+end
